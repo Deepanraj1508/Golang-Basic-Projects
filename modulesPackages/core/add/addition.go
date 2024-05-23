3fb8c087871416c8d531ad954eb7b601977e5b31
Deepanraj K <deepanraj770848@gmail.com>
2024-05-23 16:29:34 +0530

    .

---
+++ modulesPackages/core/add/addition.go
@@ -0,0 +1,9 @@
+package add
+
+import "fmt"
+
+func Addfunc() {
+	fmt.Println("Core/add Module Hello World !!!")
+	fmt.Println("")
+	fmt.Println("----------------------------")
+	}
---
+++ modulesPackages/core/function.go
@@ -0,0 +1,9 @@
+package core
+
+import "fmt"
+
+func Printfunc() {
+	fmt.Println("Core Module Hello World!")
+	fmt.Println("")
+	fmt.Println("----------------------------")
+	}
